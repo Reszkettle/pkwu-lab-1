2a7e514cefd00cecb46cd9730c456dd00bc2d384
Rechu <reszkettle@gmail.com>
2021-10-14 12:55:39 +0200

    Returning reverted string in the response body

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, Depends
 import service
 import dependencies as deps
+import schemas
 
 app = FastAPI()
 
 
-@app.get("/{string_to_reverse}")
+@app.get("/{string_to_reverse}", response_model=schemas.ReverseStringResponse)
 def reverse_string(string_to_reverse: str,
                    string_service: service.StringService = Depends(deps.get_string_service)):
-    ...
+    reversed_string = string_service.reverse_string(string_to_reverse)
+    return schemas.ReverseStringResponse(reversed_string=reversed_string)
