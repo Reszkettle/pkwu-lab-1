80c13f75c0af7d60dce958752ca7aefc40411ea2
Rechu <reszkettle@gmail.com>
2021-10-14 12:48:57 +0200

    Injected StringService dependency into reverse_string

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+import service
+import dependencies as deps
 
 app = FastAPI()
 
 
 @app.get("/{string_to_reverse}")
-def reverse_string(string_to_reverse: str):
+def reverse_string(string_to_reverse: str,
+                   string_service: service.StringService = Depends(deps.get_string_service)):
     ...
