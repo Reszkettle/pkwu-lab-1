03d921e816713c8ea008c8863ca1f7465f78077c
Rechu <reszkettle@gmail.com>
2021-10-14 12:43:16 +0200

    Created the service for reversing a string

---
+++ service.py
@@ -0,0 +1,6 @@
+class StringService:
+    def reverse_string(self, string_to_reverse: str) -> str:
+        """
+        Reverses string given in parameter
+        """
+        return string_to_reverse[::-1]
