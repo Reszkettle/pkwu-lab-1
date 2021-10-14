0e826fe08b4913c041e2d19fdc528292d5c0a4a0
Rechu <reszkettle@gmail.com>
2021-10-14 13:00:37 +0200

    Moved endpoint request handling to newly created string_router, included this router in app

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-from fastapi import FastAPI, Depends
-import services
-import dependencies as deps
-import schemas
+from fastapi import FastAPI
+import routers
 
 app = FastAPI()
-
-
-@app.get("/{string_to_reverse}", response_model=schemas.ReverseStringResponse)
-def reverse_string(string_to_reverse: str,
-                   string_service: services.StringService = Depends(deps.get_string_service)):
-    reversed_string = string_service.reverse_string(string_to_reverse)
-    return schemas.ReverseStringResponse(reversed_string=reversed_string)
+app.include_router(routers.string_router)
---
+++ routers.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends
+
+import schemas
+import services
+import dependencies as deps
+
+string_router = APIRouter(prefix="/string")
+
+
+@string_router.get("/{string_to_reverse}", response_model=schemas.ReverseStringResponse)
+def reverse_string(string_to_reverse: str,
+                   string_service: services.StringService = Depends(deps.get_string_service)):
+    reversed_string = string_service.reverse_string(string_to_reverse)
+    return schemas.ReverseStringResponse(reversed_string=reversed_string)
