ec375e7b8fa243fd076dd419163441fc165f7eda
Rechu <reszkettle@gmail.com>
2021-10-14 13:02:41 +0200

    Returning HTTP 200 status code in reverse_string on success

--- routers.py
+++ routers.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Depends
-
+from fastapi import APIRouter, Depends, status
 import schemas
 import services
 import dependencies as deps
@@ -7,7 +6,7 @@ import dependencies as deps
 string_router = APIRouter(prefix="/string")
 
 
-@string_router.get("/{string_to_reverse}", response_model=schemas.ReverseStringResponse)
+@string_router.get("/{string_to_reverse}", response_model=schemas.ReverseStringResponse, status_code=status.HTTP_200_OK)
 def reverse_string(string_to_reverse: str,
                    string_service: services.StringService = Depends(deps.get_string_service)):
     reversed_string = string_service.reverse_string(string_to_reverse)
